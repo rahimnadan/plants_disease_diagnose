4822996a9d4c432b10c2b0d5db096d87dce1ad80
Abdur Rahim <92539299+rahimnadan@users.noreply.github.com>
2024-09-03 20:18:18 +0500

    Initial commit

---
+++ chat_bot.py
@@ -0,0 +1,94 @@
+import os
+from dotenv import load_dotenv,find_dotenv
+import streamlit as st
+import google.generativeai as palm
+from langchain_google_genai import ChatGoogleGenerativeAI
+load_dotenv(find_dotenv())
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
+import os 
+from langchain_groq import ChatGroq
+
+# os.environ["GOOGLE_API_KEY"]= "AIzaSyAx-qltSJhTnuyyJgBMtBaKqz8LIeuXUrs"
+
+GROQ_API = "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"
+
+
+llm = ChatGroq(temperature=0,
+               model_name="Llama3-70b-8192",
+               api_key=GROQ_API,
+               max_tokens=254,
+               model_kwargs={
+                   "top_p": 1,
+                   "frequency_penalty": 0.0,
+                   "presence_penalty": 0.0
+               }
+               )
+
+
+system_prompt = """You are a helpful AI assistant your role is here Plant Doctor and your name is HortiCure and you are created by Kamran umer  talking with a human especially to farmers. If you do not know an answer, just say 'I don't know', do not make up an answer and must consider the given instruction after it gives a response to the user according to instructions which are given below in points.
+    1. Greet the farmer politely and ask what plant they are having trouble with.
+    2. Ask the farmer to describe the symptoms of the plant disease in detail.
+    3. Based on the farmer's description, identify the plant disease and provide a diagnosis.
+    4. If the plant disease is treatable, provide the farmer with instructions on how to treat it, including any necessary fungicides or other products.
+    5. If the plant disease is not treatable, provide the farmer with advice on how to prevent the disease from spreading to other plants.
+    6. Be patient and considerate when responding to farmer queries, and provide clear explanations.
+    7. If the user asks about a topic unrelated to Plants and plant diseases, politely say that conversation is unrelated to plants and plant diseases.
+    8. If the farmer expresses gratitude or indicates the end of the conversation, respond with a polite farewell and say that this bot was created by Engr.Zulqar Nain. Remember him in your prayers.
+
+    Remember, your primary goal is to assist and educate farmers about plant diseases. Always prioritize their learning experience and well-being."""
+
+
+
+
+prompt = ChatPromptTemplate.from_messages([
+    (
+        "system",
+        system_prompt,
+    ),
+    MessagesPlaceholder(variable_name="messages"),
+])
+
+# chat=ChatGoogleGenerativeAI(model="gemini-1.5-flash",convert_system_message_to_human=True)
+
+
+
+chain=prompt|llm
+
+
+def serialize_history(history):
+    serial_history = []
+    for message in history:
+        if message["role"] == "user":
+            serial_history.append(HumanMessage(content=message["content"]))
+        else:
+            serial_history.append(AIMessage(content=message["content"]))
+    return serial_history
+
+
+
+
+def chat():
+ 
+
+    if "messages" not in st.session_state.keys():
+        st.session_state.messages = [
+            {"role": "assistant", "content": "Ask me a questions about plants and plant diseases like Rusty Plants and Powdery Mildew Plants etc!"}
+        ]
+
+    if prompt := st.chat_input("Say something. . . . ."):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+   
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                serial_history=serialize_history(st.session_state.messages)
+                response=chain.invoke({"messages":serial_history})
+                st.write(response.content)
+                message = {"role": "assistant", "content": response.content}
+                st.session_state.messages.append(message)
---
+++ main.py
@@ -0,0 +1,135 @@
+#####################################################################
+# Author: Abdur Rahim                                               #
+# Date:28.11.2023                                                   #
+# Project title: HEALTY PLANTS                                      #
+#####################################################################
+
+import cv2
+import numpy as np
+from PIL import Image
+import streamlit as st
+import tensorflow as tf
+import os
+import base64
+from chat_bot import chat
+import os
+from dotenv import load_dotenv,find_dotenv
+import streamlit as st
+
+#------------------------------------------------------------------------------------
+#Streamlit page setup and configuration.
+st.set_page_config(page_title="HortiCure🍀",page_icon="✅",layout="wide",initial_sidebar_state="expanded")
+st.markdown("""
+<style>
+.block-container {
+  padding-top: 0.5rem;
+}
+.st-emotion-cache-16txtl3 {
+    padding: 0 1.5rem;
+}
+.sidebar .sidebar-header {
+  position: absolute;
+  top: 0;
+  width: 100%;
+  background-color: #fff;
+  z-index: 1;
+}
+</style>
+""", unsafe_allow_html=True)
+#------------------------------------------------------------------------------------
+
+
+#class of MyModel which consist of related things to traind model, loading, predicting etc..
+class MyModel:
+    def __init__(self) -> None:
+        pass
+
+    # Loading the saved model using tf.keras.models.load_model
+    def load_saved_model(self, model_path="./model.h5"):
+        model = tf.keras.models.load_model(model_path)
+        return model
+
+    # function whcich prdict a single sample
+    def single_predict(self, model, image):
+        classes = ["HEALTHY ✅", "POWDERY ❎", "RUST ❎"]
+
+        if image is not None:
+            # to Convert the PIL Image to a NumPy array
+            image = np.array(image)
+            image = image / 255.0  # Normalize the image
+            input_arr = np.array([image])
+
+            # Predict using the model
+            predictions = model.predict(input_arr)
+
+            index = np.argmax(predictions)
+            confidence = np.max(predictions)
+
+            return classes[index], confidence * 100
+        else:
+            return "Still No Image Sample uploaded", 0.0
+
+
+#------------------------------------------------------------------------------------
+#This is our UI class which is consist of related things to UI 
+class Ui:
+    def __init__(self):
+        st.title("🌿HortiCure🍀")
+
+    def ui_image_load(self):
+        st.sidebar.header("Detect your Disease Here! 🔎")
+
+
+        img_file = st.sidebar.file_uploader("Upload your image Sample Here :floppy_disk:", type=["jpg", "jpeg","png",])
+        if img_file is not None:
+            image = self.read_file_as_image(img_file)  # Use self here
+            st.sidebar.image(image, caption="Uploaded Sample", use_column_width=True)
+            return image
+
+
+    def read_file_as_image(self, data):
+        image = Image.open(data)
+
+        if image.mode == 'RGBA':
+            # Convert RGBA to RGB
+            image = image.convert('RGB')
+
+        image = image.resize((256, 256))  # Resize the image to the expected dimensions
+        image = np.array(image)
+        return image 
+    def show_result(self,label,confidence):
+        if label=="HEALTHY ✅":
+            st.balloons()
+        st.sidebar.success(f"HEALTH STATUS: {label}")
+        st.sidebar.error(f"CONFEDINCE: {int(confidence)}%")
+
+    ####chatbot-Section#######
+    def chat_bot_interface(self):
+        chat()
+
+#------------------------------------------------------------------------------------
+    
+if __name__ == "__main__":
+    # class instances
+    model_obj = MyModel()
+    ui = Ui()
+
+    # to load the saved model
+    model = model_obj.load_saved_model()
+
+    # to load the image from the source
+    imagee = ui.ui_image_load()
+
+    # to predict the loaded image....
+    label, confidence = model_obj.single_predict(model, imagee)
+    ui.show_result(label, confidence)
+
+    # Run the chat_bot function
+    ui.chat_bot_interface()
+
+    st.sidebar.write("Developed by Abdur Rahim")
+    
+    st.write("Copyright ©2024  abdurahimkhan145@gmail.com")
+    
+
+#------------------------------------------------------------------------------------
